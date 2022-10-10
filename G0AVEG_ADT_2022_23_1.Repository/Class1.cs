8e44b523a088a8b2babbb627e61142a228992415
MPSeb <mopeti00@gmail.com>
2022-10-10 12:30:18 +0200

    Add project files.

---
+++ G0AVEG_ADT_2022_23_1.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace G0AVEG_ADT_2022_23_1.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ G0AVEG_ADT_2022_23_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace G0AVEG_ADT_2022_23_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
