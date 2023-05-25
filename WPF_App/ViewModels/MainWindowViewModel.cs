af7dc5062af62e04cd021b15d0c8dce242113e5e
MPSeb <mopeti00@gmail.com>
2023-05-25 14:11:46 +0200

    Added Wood and Furniture stuff

--- WPF_App/ViewModels/MainWindowViewModel.cs
+++ WPF_App/ViewModels/MainWindowViewModel.cs
@@ -16,21 +16,65 @@ namespace WPF_App.ViewModels
 {
     class MainWindowViewModel : ObservableRecipient
     {
+        public ICommand WoodsIdsForRetailer { get; set; }
+        public ICommand DoesRetailerSellWood { get; set; }
+        public ICommand AvgWoodPriceOfRetailer { get; set; }
+        public ICommand AverageFurnPerRetailer { get; set; }
+        public ICommand WoodUsedInFurnBelowPrice { get; set; }
+
+
 
         public ICommand CreateFurniture { get; set; }
         public ICommand RemoveFurniture { get; set; }
-        public ICommand EditFurniture { get; set; }
-        public ICommand WoodsIdsForRetailer{get; set; }
-        public ICommand DoesRetailerSellWood{get;set; }
-        public ICommand AvgWoodPriceOfRetailer{get;set; }
-        public ICommand AverageFurnPerRetailer { get; set; }
-        public ICommand WoodUsedInFurnBelowPrice{get; set; }
+        public ICommand UpdateFurniture { get; set; }
         public ICommand CreateRetailer { get; set; }
         public ICommand RemoveRetailer { get; set; }
         public ICommand UpdateRetailer { get; set; }
         public ICommand CreateWood { get; set; }
         public ICommand RemoveWood { get; set; }
         public ICommand UpdateWood { get; set; }
+
+        private Wood selectedWood;
+        public Wood SelectedWood
+        {
+            get { return selectedWood; }
+            set
+            {
+                SetProperty(ref selectedWood, value);
+                if (selectedWood != null)
+                {
+                    Name = selectedWood.Name;
+                }
+                (RemoveWood as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+        private string nameWood;
+        public string NameWood
+        {
+            get { return nameWood; }
+            set
+            {
+                SetProperty(ref nameWood, value);
+                (CreateWood as RelayCommand).NotifyCanExecuteChanged();
+                (UpdateWood as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+        private Furniture selectedFurniture;
+        public Furniture SelectedFurniture
+        {
+            get { return selectedFurniture; }
+            set
+            {
+                SetProperty(ref selectedFurniture, value);
+                if(selectedFurniture != null)
+                {
+                    Name = selectedFurniture.Name;
+                }
+                (RemoveFurniture as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
         private Retailer selectedRetailer;
         public Retailer SelectedRetailer
         {
@@ -65,8 +109,12 @@ namespace WPF_App.ViewModels
         public MainWindowViewModel(RestService restService)
         {
             this.restService = restService;
-            retailers.Add(new Retailer { Name = "test", Id = 1 });
-            CreateRetailer = new RelayCommand(async () => { await restService.Post(new Retailer { Name = Name }, "retailer"); DownloadRetailers(); }, () => !string.IsNullOrEmpty(Name));
+            retailers.Add(new Retailer { Name = "test", Id = 100 });
+            woods.Add(new Wood { Name = "test", Id = 100 });
+            furnitures.Add(new Furniture { Name = "test", Id = 100 });
+
+
+            CreateRetailer = new RelayCommand(async () => { await restService.Post(new Retailer { Name = Name }, "retailer"); DownloadData(); }, () => !string.IsNullOrEmpty(Name));
             UpdateRetailer = new RelayCommand(() =>
             {
                 SelectedRetailer.Name = Name;
@@ -77,14 +125,50 @@ namespace WPF_App.ViewModels
                 restService.Delete(selectedRetailer.Id, "retailer");
                 retailers.Remove(SelectedRetailer);
             }, () => SelectedRetailer != null);
-            DownloadRetailers();
+
+            CreateWood = new RelayCommand(async () => { await restService.Post(new Wood { Name = Name }, "wood"); DownloadData(); }, () => !string.IsNullOrEmpty(Name));
+            UpdateWood = new RelayCommand(() =>
+            {
+                SelectedWood.Name = Name;
+                restService.Put(SelectedWood, "wood");
+            }, () => !string.IsNullOrEmpty(Name));
+            RemoveWood = new RelayCommand(() =>
+            {
+                restService.Delete(selectedWood.Id, "wood");
+                woods.Remove(SelectedWood);
+            }, () => SelectedRetailer != null);
+
+            CreateFurniture = new RelayCommand(async () => { await restService.Post(new Furniture { Name = Name }, "furniture"); DownloadData(); }, () => !string.IsNullOrEmpty(Name));
+            UpdateFurniture = new RelayCommand(() =>
+            {
+                SelectedFurniture.Name = Name;
+                restService.Put(SelectedFurniture, "furniture");
+            }, () => !string.IsNullOrEmpty(Name));
+            RemoveFurniture = new RelayCommand(() =>
+            {
+                restService.Delete(selectedFurniture.Id, "furniture");
+                furnitures.Remove(SelectedFurniture);
+            }, () => SelectedFurniture != null);
+
+            DownloadData();
+
         }
-        private void DownloadRetailers()
+        private void DownloadData()
         {
+            woods.Clear();
+            furnitures.Clear();
             retailers.Clear();
             foreach(var retailer in restService.Get<Retailer>("retailer"))
             {
                 retailers.Add(retailer);
+            }
+            foreach(var wood in restService.Get<Wood>("wood"))
+            {
+                woods.Add(wood);
+            }
+            foreach(var furniture in restService.Get<Furniture>("furniture"))
+            {
+                furnitures.Add(furniture);
             }
         }
         public MainWindowViewModel() : this(Ioc.Default.GetService<RestService>())
